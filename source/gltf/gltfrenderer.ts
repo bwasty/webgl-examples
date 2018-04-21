f6852f5f6c15f2e26050b66a1d0bdafce7cfb721
Benjamin Wasty <benny.wasty@gmail.com>
2018-04-21 23:42:40 +0200

    add simple shader; some basic initialization

--- source/gltf/gltfrenderer.ts
+++ source/gltf/gltfrenderer.ts
@@ -1,16 +1,30 @@
-import { Context, Invalidate, MouseEventProvider, Renderer } from 'webgl-operate';
+import { Context, Invalidate, MouseEventProvider, Program, Renderer, Shader } from 'webgl-operate';
+import { Primitive } from './primitive';
 
 
 export class GltfRenderer extends Renderer {
+    private _program: Program;
+
+    public primitive: Primitive;
+
     protected onInitialize(
         context: Context,
         callback: Invalidate,
         mouseEventProvider: MouseEventProvider | undefined): boolean {
 
+        const gl = this._context.gl;
+
+        const vert = new Shader(this._context, gl.VERTEX_SHADER, 'simple.vert');
+        vert.initialize(require('./simple.vert'));
+        const frag = new Shader(this._context, gl.FRAGMENT_SHADER, 'simple.frag');
+        frag.initialize(require('./simple.frag'));
+        this._program = new Program(this._context);
+        this._program.initialize([vert, frag]);
+
         return true;
     }
     protected onUninitialize(): void {
-        console.error('Method not implemented.');
+        this._program.uninitialize();
     }
     protected onUpdate(): boolean {
         console.error('Method not implemented.');
