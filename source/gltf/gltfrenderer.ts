0cf50a5622b954019fefee81f22cc11d96b2f965
Benjamin Wasty <benny.wasty@gmail.com>
2018-04-22 21:12:08 +0200

    mostly finish/fix renderer

--- source/gltf/gltfrenderer.ts
+++ source/gltf/gltfrenderer.ts
@@ -1,9 +1,18 @@
 import { vec3 } from 'gl-matrix';
-import { Camera, Context, Invalidate, MouseEventProvider, Navigation, Program, Renderer, Shader } from 'webgl-operate';
+import { BlitPass, Camera, Context, DefaultFramebuffer,
+    Framebuffer, Invalidate, MouseEventProvider, Navigation, Program,
+    Renderbuffer, Renderer, Shader, Texture2 } from 'webgl-operate';
 import { Primitive } from './primitive';
 
 export class GltfRenderer extends Renderer {
-    private _program: Program;
+    // FBO and Blit
+    protected _defaultFBO: DefaultFramebuffer;
+    protected _colorRenderTexture: Texture2;
+    protected _depthRenderbuffer: Renderbuffer;
+    protected _intermediateFBO: Framebuffer;
+    protected _blit: BlitPass;
+
+    protected _program: Program;
     protected _uViewProjection: WebGLUniformLocation;
     protected _uModel: WebGLUniformLocation;
     protected _aVertex: GLuint;
@@ -20,6 +29,7 @@ export class GltfRenderer extends Renderer {
         mouseEventProvider: MouseEventProvider): boolean {
 
         const gl = this._context.gl;
+        const gl2facade = this._context.gl2facade;
 
         const vert = new Shader(this._context, gl.VERTEX_SHADER, 'simple.vert');
         vert.initialize(require('./simple.vert'));
@@ -38,11 +48,29 @@ export class GltfRenderer extends Renderer {
         this._camera.up = vec3.fromValues(0.0, 1.0, 0.0);
         this._camera.eye = vec3.fromValues(0.0, 0.0, 2.0);
         this._camera.near = 0.1;
-        this._camera.far = 8.0;
+        this._camera.far = 20.0;
 
         // Initialize navigation
         this._navigation = new Navigation(callback, mouseEventProvider);
         this._navigation.camera = this._camera;
+
+        // Initialize FBO & BlitPass
+        this._defaultFBO = new DefaultFramebuffer(this._context, 'DefaultFBO');
+        this._defaultFBO.initialize();
+        this._colorRenderTexture = new Texture2(this._context, 'ColorRenderTexture');
+        this._colorRenderTexture.initialize(480, 270,
+            this._context.isWebGL2 ? gl.RGBA8 : gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE);
+        this._depthRenderbuffer = new Renderbuffer(this._context, 'DepthRenderbuffer');
+        this._depthRenderbuffer.initialize(480, 270, gl.DEPTH_COMPONENT16);
+        this._intermediateFBO = new Framebuffer(this._context, 'IntermediateFBO');
+        this._intermediateFBO.initialize([[gl2facade.COLOR_ATTACHMENT0, this._colorRenderTexture]
+            , [gl.DEPTH_ATTACHMENT, this._depthRenderbuffer]]);
+        this._blit = new BlitPass(this._context);
+        this._blit.initialize();
+        this._blit.framebuffer = this._intermediateFBO;
+        this._blit.readBuffer = gl2facade.COLOR_ATTACHMENT0;
+        this._blit.drawBuffer = gl.BACK;
+        this._blit.target = this._defaultFBO;
 
         setTimeout(() => {
             this.clearColor = [0.0, 0.0, 1.0, 1.0];
@@ -50,12 +78,22 @@ export class GltfRenderer extends Renderer {
 
         return true;
     }
+
     protected onUninitialize(): void {
+        this.primitive.uninitialize();
         this._program.uninitialize();
+
+        this._intermediateFBO.uninitialize();
+        this._defaultFBO.uninitialize();
+        this._colorRenderTexture.uninitialize();
+        this._depthRenderbuffer.uninitialize();
+        this._blit.uninitialize();
     }
+
     protected onUpdate(): boolean {
         // Resize
         if (this._altered.frameSize) {
+            this._intermediateFBO.resize(this._frameSize[0], this._frameSize[1]);
             this._camera.viewport = [this._frameSize[0], this._frameSize[1]];
         }
         if (this._altered.canvasSize) {
@@ -65,7 +103,7 @@ export class GltfRenderer extends Renderer {
         // Update clear color
         if (this._altered.clearColor) {
             const c = this._clearColor;
-            this.context.gl.clearColor(c[0], c[1], c[2], c[3]);
+            this._intermediateFBO.clearColor(this._clearColor);
         }
 
         this._navigation.update();
@@ -78,19 +116,36 @@ export class GltfRenderer extends Renderer {
         // If anything has changed, render a new frame
         return altered;
     }
+
     protected onPrepare(): void { }
+
     protected onFrame(frameNumber: number): void {
         const gl = this._context.gl;
 
+        // Bind FBO
+        this._intermediateFBO.bind();
+        this._intermediateFBO.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT, false, false);
+
         // Set viewport
         gl.viewport(0, 0, this._frameSize[0], this._frameSize[1]);
+
+        this._program.bind();
+        gl.uniformMatrix4fv(this._uViewProjection, gl.GL_FALSE, this._camera.viewProjection);
+        // TODO!!: model matrix - identity?
 
         if (this.primitive) {
             this.primitive.bind();
             this.primitive.draw();
         }
+
+        this._program.unbind();
+
+        // Unbind FBO
+        this._intermediateFBO.unbind();
     }
     protected onSwap(): void {
-        // TODO!!: this.invalidate()? why?
+         // Blit into framebuffer
+         this._blit.frame();
+         this.invalidate(); // TODO!: why?
     }
 }
--- source/gltf/simple.vert
+++ source/gltf/simple.vert
@@ -5,28 +5,30 @@
     // TODO!!
 #else
     layout (location = 0) in vec3 a_vertex;
-    layout (location = 1) in vec3 a_normal;
-    layout (location = 2) in vec4 a_tangent;
-    layout (location = 3) in vec2 a_texCoords_0;
-    layout (location = 4) in vec2 a_texCoords_1;
-    layout (location = 5) in vec3 a_color;
+    // layout (location = 1) in vec3 a_normal;
+    // layout (location = 2) in vec4 a_tangent;
+    // layout (location = 3) in vec2 a_texCoords_0;
+    // layout (location = 4) in vec2 a_texCoords_1;
+    // layout (location = 5) in vec3 a_color;
 #endif
 
 // out vec3 Normal;
 // out vec4 Tangent;
-out vec2 TexCoords_0;
+// out vec2 TexCoords_0;
 // out vec2 TexCoords_1;
 // out vec3 Color;
 
-uniform mat4 u_model;
+// uniform mat4 u_model;
 uniform mat4 u_viewProjection;
 
 void main()
 {
     // Normal = a_normal; // TODO: transform
     // Tangent = a_tangent;
-    TexCoords_0 = a_texCoords_0;
+    // TexCoords_0 = a_texCoords_0;
     // TexCoords_1 = a_texCoords_1;
     // Color = a_color;
-    gl_Position = u_viewProjection * u_model * vec4(a_vertex, 1.0);
+    gl_Position = u_viewProjection
+        // * u_model
+        * vec4(a_vertex, 1.0);
 }
