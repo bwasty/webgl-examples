18ea237282c6d1588228b0ecfec0f70992d07ea2
Benjamin Wasty <benny.wasty@gmail.com>
2018-04-22 09:35:14 +0200

    add camera, navigation

--- source/gltf/gltfrenderer.ts
+++ source/gltf/gltfrenderer.ts
@@ -1,16 +1,24 @@
-import { Context, Invalidate, MouseEventProvider, Program, Renderer, Shader } from 'webgl-operate';
+import { Camera, Context, Invalidate, MouseEventProvider, Navigation, Program, Renderer, Shader } from 'webgl-operate';
 import { Primitive } from './primitive';
+import { vec3 } from 'gl-matrix';
 
 
 export class GltfRenderer extends Renderer {
     private _program: Program;
+    protected _uViewProjection: WebGLUniformLocation;
+    protected _uModel: WebGLUniformLocation;
+    protected _aVertex: GLuint;
+
+    // Camera and navigation
+    protected _camera: Camera;
+    protected _navigation: Navigation;
 
     public primitive: Primitive;
 
     protected onInitialize(
         context: Context,
         callback: Invalidate,
-        mouseEventProvider: MouseEventProvider | undefined): boolean {
+        mouseEventProvider: MouseEventProvider): boolean {
 
         const gl = this._context.gl;
 
@@ -20,6 +28,22 @@ export class GltfRenderer extends Renderer {
         frag.initialize(require('./simple.frag'));
         this._program = new Program(this._context);
         this._program.initialize([vert, frag]);
+        this._aVertex = this._program.attribute('a_vertex', 0);
+
+        this._uViewProjection = this._program.uniform('u_viewProjection');
+        this._uModel = this._program.uniform('u_model');
+
+        // Initialize camera
+        this._camera = new Camera();
+        this._camera.center = vec3.fromValues(0.0, 0.0, 0.0);
+        this._camera.up = vec3.fromValues(0.0, 1.0, 0.0);
+        this._camera.eye = vec3.fromValues(0.0, 0.0, 2.0);
+        this._camera.near = 0.1;
+        this._camera.far = 8.0;
+
+        // Initialize navigation
+        this._navigation = new Navigation(callback, mouseEventProvider);
+        this._navigation.camera = this._camera;
 
         return true;
     }
@@ -30,9 +54,7 @@ export class GltfRenderer extends Renderer {
         console.error('Method not implemented.');
         return false; // whether to redraw
     }
-    protected onPrepare(): void {
-        console.error('Method not implemented.');
-    }
+    protected onPrepare(): void { }
     protected onFrame(frameNumber: number): void {
         console.error('Method not implemented.');
     }
