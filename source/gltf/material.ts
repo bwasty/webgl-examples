2fa5297d9688c2e4b94d7c8dcc824dc276c0e62a
Benjamin Wasty <benny.wasty@gmail.com>
2018-04-27 17:19:06 +0200

    mostly finish Material.fromGltf

--- source/gltf/material.ts
+++ source/gltf/material.ts
@@ -2,10 +2,14 @@ import { vec3, vec4 } from 'gl-matrix';
 import { gltf as GLTF, GltfAsset } from 'gltf-loader-ts';
 import { Context, Texture2 } from 'webgl-operate';
 
+export type AlphaMode = 'OPAQUE' | 'MASK' | 'BLEND';
+
 export class Material {
     name: string;
 
-    // pbr_metallic_roughness properties (defaults from spec)
+    // NOTE: all defaults are from the spec
+
+    // pbr_metallic_roughness properties
     baseColorFactor: vec4 = vec4.fromValues(1, 1, 1, 1);
     baseColorTexture: Texture2;
     metallicFactor = 1;
@@ -17,31 +21,72 @@ export class Material {
 
     occlusionTexture: Texture2;
     occlusionStrength: number;
-    emissiveFactor: vec3;
+    emissiveFactor: vec3 = vec3.fromValues(0, 0, 0);
     emissiveTexture: Texture2;
 
-    alphaCutoff: number;
-    alphaMode: 'OPAQUE' | 'MASK' | 'BLEND';
+    alphaCutoff = 0.5;
+    alphaMode: AlphaMode = 'OPAQUE';
 
-    doubleSided: boolean;
+    doubleSided = false;
 
-    static fromGltf(gMaterial: GLTF.Material, asset: GltfAsset, context: Context): Material {
+    static async fromGltf(gMaterial: GLTF.Material, asset: GltfAsset, context: Context): Promise<Material> {
         const mat = new Material();
         mat.name = gMaterial.name;
-        if (gMaterial.pbrMetallicRoughness) {
-            const pbr = gMaterial.pbrMetallicRoughness;
+        const pbr = gMaterial.pbrMetallicRoughness;
+        // TODO!!: load all textures in parallel -> Promise.all...
+        if (pbr) {
             if (pbr.baseColorFactor) {
                 mat.baseColorFactor = vec4.fromValues.apply(undefined, pbr.baseColorFactor);
-                // mat.baseColorTexture =
-                // pbr.
+            }
+            if (pbr.baseColorTexture) {
+                mat.baseColorTexture = await this.loadTexture(pbr.baseColorTexture, asset, context);
+            }
+            if (pbr.metallicFactor !== undefined) {
+                mat.metallicFactor = pbr.metallicFactor;
+            }
+            if (pbr.roughnessFactor !== undefined) {
+                mat.roughnessFactor = pbr.roughnessFactor;
+            }
+            if (pbr.metallicRoughnessTexture) {
+                mat.metallicRoughnessTexture = await this.loadTexture(pbr.metallicRoughnessTexture, asset, context);
             }
         }
-        // TODO!!!
+
+        const normalTexInfo = gMaterial.normalTexture;
+        if (normalTexInfo) {
+            mat.normalTexture = await this.loadTexture(normalTexInfo, asset, context);
+            mat.normalScale = normalTexInfo.scale || 1;
+        }
+
+        const occTexInfo = gMaterial.occlusionTexture;
+        if (occTexInfo) {
+            mat.occlusionTexture = await this.loadTexture(occTexInfo, asset, context);
+            mat.occlusionStrength = occTexInfo.strength || 1;
+        }
+
+        if (gMaterial.emissiveTexture) {
+            mat.emissiveTexture = await this.loadTexture(gMaterial.emissiveTexture, asset, context);
+        }
+        if (gMaterial.emissiveFactor) {
+            mat.emissiveFactor = vec3.fromValues.apply(undefined, gMaterial.emissiveFactor);
+        }
+
+        if (gMaterial.alphaCutoff !== undefined) {
+            mat.alphaCutoff = gMaterial.alphaCutoff;
+        }
+
+        if (gMaterial.alphaMode) {
+            mat.alphaMode = gMaterial.alphaMode as AlphaMode;
+        }
+        if (gMaterial.doubleSided !== undefined) {
+            mat.doubleSided = gMaterial.doubleSided;
+        }
 
         return mat;
     }
 
-    static async loadTexture(texInfo: GLTF.TextureInfo, asset: GltfAsset, context: Context): Promise<Texture2> {
+    static async loadTexture(texInfo: GLTF.TextureInfo | GLTF.MaterialNormalTextureInfo,
+            asset: GltfAsset, context: Context): Promise<Texture2> {
         const gl = context.gl;
         const gltf = asset.gltf;
         const texCoord = texInfo.texCoord || 0; // TODO!: use/handle
@@ -54,16 +99,19 @@ export class Material {
         } else {
             // spec: when undefined, a sampler with repeat wrapping and auto filtering should be used.
             sampler = {
-                wrapS: 10497, // repeat
-                wrapT: 10497,
+                wrapS: gl.REPEAT,
+                wrapT: gl.REPEAT,
             };
         }
 
         const tex2 = new Texture2(context); // TODO: identifier?
-        tex2.initialize(image.width, image.height, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE); // TODO!: internalFormat? type?
-        // TODO!!: bind/unbind?
-        tex2.wrap(sampler.wrapS, sampler.wrapT);
-        tex2.filter(sampler.magFilter, sampler.minFilter);
-        tex2.data(image);
+        tex2.initialize(image.width, image.height, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE); // TODO: internalFormat? type?
+        tex2.wrap(sampler.wrapS || gl.REPEAT, sampler.wrapT || gl.REPEAT, true, false);
+        tex2.filter(sampler.magFilter || gl.LINEAR, sampler.minFilter || gl.LINEAR, false, false);
+        tex2.data(image, false, true);
+
+        // TODO!!: NPOT, mipmap handling
+
+        return tex2;
     }
 }
