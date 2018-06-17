ea7b0eaf9188a79116fa56b903d5c72e0572b9f3
Benjamin Wasty <benny.wasty@gmail.com>
2018-06-17 14:26:47 +0200

    start on webxr initialization

--- source/webxr/example.ts
+++ source/webxr/example.ts
@@ -3,12 +3,9 @@ import * as gloperate from 'webgl-operate';
 
 import { Asset } from '../gltf/asset';
 import { WebXRRenderer } from './webxrrenderer';
+import { supportsXR } from 'webgl-operate';
 
 // tslint:disable:no-console
-
-function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
 
 async function loadScene(gAsset: GltfAsset, renderer: WebXRRenderer) {
     console.time('asset.getScene');
@@ -46,26 +43,51 @@ function getQueryParam(param: string): string | undefined {
     }
 }
 
+
 async function onload() {
     // TODO!!: HACK - see https://github.com/cginternals/webgl-operate/issues/68
-    (gloperate.Context as any).CONTEXT_ATTRIBUTES.depth = true;
-    const canvas = new gloperate.Canvas('example-canvas');
-    const context = canvas.context;
-    const renderer = new WebXRRenderer();
-    canvas.renderer = renderer;
+    // (gloperate.Context as any).CONTEXT_ATTRIBUTES.depth = true;
+    // const canvas = new gloperate.Canvas('example-canvas');
+    // const context = canvas.context;
+    // const renderer = new WebXRRenderer();
+    // canvas.renderer = renderer;
 
+    const messageEl = document.getElementById('message')!;
+    const errorMessage = (msg: string) => messageEl.innerHTML = msg;
+    const xrButton = document.getElementById('xr-button') as HTMLButtonElement;
+
+    if (!supportsXR()) {
+        errorMessage('Your browser does not support WebXR.');
+        return;
+    }
+
+    const xrc = new gloperate.XRController({ immersive: true });
+    // TODO!!: need to distinguish device / session support cases...
+    if (!await xrc.initialize()) {
+        errorMessage('Failed to initialize WebXR - do you have a device connected?');
+        return;
+    }
+    errorMessage('');
+    xrButton.disabled = false;
+
+    xrButton.click = () => {
+        errorMessage('Requesting sessions...');
+        xrc.requestSession();
+    };
+
+    // TODO!!!: need to get canvas from xrc to set renderer?
     const loader = new GltfLoader();
 
-    // TODO!!!
+    // const uri = 'https://raw.githubusercontent.com/immersive-web/webxr-samples/master/media/gltf/space/space.gltf';
     // loadGltf(loader, uri, renderer);
 
-    canvas.element.addEventListener('dblclick', () => gloperate.viewer.Fullscreen.toggle(canvas.element));
-    canvas.element.addEventListener('touchstart', () => gloperate.viewer.Fullscreen.toggle(canvas.element));
+    // canvas.element.addEventListener('dblclick', () => gloperate.viewer.Fullscreen.toggle(canvas.element));
+    // canvas.element.addEventListener('touchstart', () => gloperate.viewer.Fullscreen.toggle(canvas.element));
 
-    // export variables
-    (window as any)['canvas'] = canvas;
-    (window as any)['context'] = context;
-    (window as any)['renderer'] = renderer;
+    // // export variables
+    // (window as any)['canvas'] = canvas;
+    // (window as any)['context'] = context;
+    // (window as any)['renderer'] = renderer;
 }
 
 if (window.document.readyState === 'complete') {
