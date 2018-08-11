a322b66d48e8e879787b346513f6a3a8cf037d69
Benjamin Wasty <benny.wasty@gmail.com>
2018-08-12 01:52:24 +0200

    extend/change standalone webpack setup

--- standalone/webpack-minimal/src/index.ts
+++ standalone/webpack-minimal/src/index.ts
@@ -1,1 +1,8 @@
-console.log(42)
+import './styles.css';
+
+// import { Canvas } from 'webgl-operate/source/canvas';
+import { Canvas } from 'webgl-operate';
+
+const canvasEl = document.querySelector('canvas')!;
+const canvas = new Canvas(canvasEl);
+(window as any).canvas = canvas;
--- standalone/webpack-minimal/webpack.config.js
+++ standalone/webpack-minimal/webpack.config.js
@@ -1,31 +1,37 @@
-// `CheckerPlugin` is optional. Use it if you want async error reporting.
-// We need this plugin to detect a `--watch` mode. It may be removed later
-// after https://github.com/webpack/webpack/issues/3460 will be resolved.
-const { CheckerPlugin } = require('awesome-typescript-loader')
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-
-  // Currently we need to add '.ts' to the resolve.extensions array.
+  mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
+    extensions: ['.ts', '.tsx', '.js']
   },
-
-  // Source maps support ('inline-source-map' also works)
-  devtool: 'source-map',
-
-  // Add the loader for .ts files.
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
   plugins: [
-      new CheckerPlugin(),
-      new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
   ]
 };
+
+if (process.env.ANALYZE) {
+  const analyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+  module.exports.cache = false;
+  module.exports.plugins.push(new analyzer());
+}
