4bf5a4bf7af9f077e162d16bd9e840a4730acce0
Benjamin Wasty <benny.wasty@gmail.com>
2018-09-21 17:13:31 +0200

    cleanup; declare textures, fbos

--- source/metaballs/metaballrenderer.ts
+++ source/metaballs/metaballrenderer.ts
@@ -2,113 +2,128 @@
 import {
     Camera, Context, Invalidate, MouseEventProvider, Navigation, Program, Renderer,
     Shader,
+    Texture2,
     TextureCube,
     vec3,
     Wizard,
+    Framebuffer,
+    NdcFillingTriangle,
 } from 'webgl-operate';
 import { Skybox } from '../camera-navigation/skybox';
 
 
 export class MetaballRenderer extends Renderer {
-    protected _programParticleStep: Program;
-    protected _programMetaballs: Program;
-    protected _programBlurH: Program;
-    protected _programBlurV: Program;
-    protected _programGlow: Program;
+    programParticleStep: Program;
+    programMetaballs: Program;
+    programBlurH: Program;
+    programBlurV: Program;
+    programGlow: Program;
+
+    positions: Texture2;
+    velocities: Texture2;
+    materials: [Texture2, Texture2];
+    // forces: Texture3;
+    color: Texture2;
+    glows: [Texture2, Texture2];
+
+    stepFBO: Framebuffer;
+    colorFBO: Framebuffer;
+    glowFBO: Framebuffer;
+    // TODO: depth?
+
+    ndcTriangle: NdcFillingTriangle;
 
     // Camera and navigation
-    protected _camera: Camera;
-    protected _navigation: Navigation;
+    camera: Camera;
+    navigation: Navigation;
 
     // SkyBox
-    protected _cubeMap: TextureCube;
-    protected _skyBox: Skybox;
-    protected _cubeMapChanged: boolean;
+    cubeMap: TextureCube;
+    skyBox: Skybox;
+    cubeMapChanged: boolean;
 
-    protected _lastStepTimestamp: number;
+    lastStepTimestamp: number;
 
-    protected onUpdate(): boolean {
+    onUpdate(): boolean {
         // Update camera navigation (process events)
-        this._navigation.update();
+        this.navigation.update();
 
         // Reset state
-        const altered = this._altered.any || this._camera.altered || this._cubeMapChanged;
+        const altered = this._altered.any || this.camera.altered || this.cubeMapChanged;
         this._altered.reset();
-        this._camera.altered = false;
-        this._cubeMapChanged = false;
+        this.camera.altered = false;
+        this.cubeMapChanged = false;
 
         // If anything has changed, render a new frame
         return altered;
     }
 
-    protected onPrepare(): void {
+    onPrepare(): void {
     }
 
-    protected onFrame(frameNumber: number): void {
-        const gl = this._context.gl;
+    onFrame(frameNumber: number): void {
+        const gl = this.context.gl;
 
         gl.viewport(0, 0, this._frameSize[0], this._frameSize[1]);
-
-        ///
         gl.disable(gl.DEPTH_TEST);
 
-        this._programMetaballs.bind();
+        this.programMetaballs.bind();
 
         // Render skybox
-        this._skyBox.frame();
+        this.skyBox.frame();
     }
 
-    protected step() {
+    step() {
         const now = performance.now();
-        const delta = now - this._lastStepTimestamp;
-        this._lastStepTimestamp = now;
+        const delta = now - this.lastStepTimestamp;
+        this.lastStepTimestamp = now;
     }
 
-    protected onSwap(): void {
+    onSwap(): void {
     }
 
 
-    protected onInitialize(context: Context, callback: Invalidate, mouseEventProvider: MouseEventProvider): boolean {
-        const gl = this._context.gl;
+    onInitialize(context: Context, callback: Invalidate, mouseEventProvider: MouseEventProvider): boolean {
+        const gl = context.gl;
 
-        this._programParticleStep = this.createProgram('particle_step.vert', 'particle_step.frag');
-        this._programMetaballs = this.createProgram('particle_draw_5.vert', 'particle_draw_5_3.frag');
-        this._programBlurH = this.createProgram('particle_glow_5_4.vert', 'gaussh.frag');
-        this._programBlurV = this.createProgram('particle_glow_5_4.vert', 'gaussv.frag');
-        this._programGlow = this.createProgram('particle_glow_5_4.vert', 'particle_glow_5_4.frag');
+        this.programParticleStep = this.createProgram('particle_step.vert', 'particle_step.frag');
+        this.programMetaballs = this.createProgram('particle_draw_5.vert', 'particle_draw_5_3.frag');
+        this.programBlurH = this.createProgram('particle_glow_5_4.vert', 'gaussh.frag');
+        this.programBlurV = this.createProgram('particle_glow_5_4.vert', 'gaussv.frag');
+        this.programGlow = this.createProgram('particle_glow_5_4.vert', 'particle_glow_5_4.frag');
 
         // Initialize camera
-        this._camera = new Camera();
-        this._camera.center = vec3.fromValues(0.0, 0.0, 0.0);
-        this._camera.up = vec3.fromValues(0.0, 1.0, 0.0);
-        this._camera.eye = vec3.fromValues(0.0, 0.0, 2.0);
-        this._camera.near = 0.1;
-        this._camera.far = 8.0;
+        this.camera = new Camera();
+        this.camera.center = vec3.fromValues(0.0, 0.0, 0.0);
+        this.camera.up = vec3.fromValues(0.0, 1.0, 0.0);
+        this.camera.eye = vec3.fromValues(0.0, 0.0, 2.0);
+        this.camera.near = 0.1;
+        this.camera.far = 8.0;
 
         // Initialize navigation
-        this._navigation = new Navigation(callback, mouseEventProvider);
-        this._navigation.camera = this._camera;
+        this.navigation = new Navigation(callback, mouseEventProvider);
+        this.navigation.camera = this.camera;
 
         // Initialize skyBox
         const internalFormatAndType = Wizard.queryInternalTextureFormat(this._context, gl.RGB, Wizard.Precision.byte);
-        this._cubeMap = new TextureCube(this._context);
-        this._cubeMap.initialize(1024, internalFormatAndType[0], gl.RGB, internalFormatAndType[1]);
+        this.cubeMap = new TextureCube(this._context);
+        this.cubeMap.initialize(1024, internalFormatAndType[0], gl.RGB, internalFormatAndType[1]);
 
-        this._skyBox = new Skybox();
-        this._skyBox.initialize(this._context, this._camera, this._cubeMap);
+        this.skyBox = new Skybox();
+        this.skyBox.initialize(this._context, this.camera, this.cubeMap);
 
-        this._cubeMap.load({
+        this.cubeMap.load({
             positiveX: 'data/env_cube_px.png', negativeX: 'data/env_cube_nx.png',
             positiveY: 'data/env_cube_py.png', negativeY: 'data/env_cube_ny.png',
             positiveZ: 'data/env_cube_pz.png', negativeZ: 'data/env_cube_nz.png',
         }).then(() => this.invalidate(true));
 
-        this._lastStepTimestamp = performance.now();
+        this.lastStepTimestamp = performance.now();
 
         return true;
     }
 
-    protected createProgram(vertexFile: string, fragmentFile: string): Program {
+    createProgram(vertexFile: string, fragmentFile: string): Program {
         const vert = new Shader(this._context, WebGLRenderingContext.VERTEX_SHADER, vertexFile);
         vert.initialize(require('./' + vertexFile));
         const frag = new Shader(this._context, WebGLRenderingContext.FRAGMENT_SHADER, fragmentFile);
@@ -118,8 +133,7 @@ export class MetaballRenderer extends Renderer {
         return program;
     }
 
-    protected onUninitialize(): void {
+    onUninitialize(): void {
     }
-
 }
 
