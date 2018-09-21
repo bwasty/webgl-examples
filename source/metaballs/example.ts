7816525bb6b67a88760ad3b9f81e1c0e489c91da
Benjamin Wasty <benny.wasty@gmail.com>
2018-09-21 14:05:54 +0200

    stub metaball example

---
+++ source/metaballs/example.ts
@@ -0,0 +1,27 @@
+
+
+import * as gloperate from 'webgl-operate';
+
+import { MetaballRenderer } from './metaballrenderer';
+
+
+function onload() {
+    const canvas = new gloperate.Canvas('example-canvas');
+    const context = canvas.context;
+    const renderer = new MetaballRenderer();
+    canvas.renderer = renderer;
+
+    canvas.element.addEventListener('click', () => { gloperate.viewer.Fullscreen.toggle(canvas.element); });
+    canvas.element.addEventListener('touchstart', () => { gloperate.viewer.Fullscreen.toggle(canvas.element); });
+
+    // export variables
+    (window as any)['canvas'] = canvas;
+    (window as any)['context'] = context;
+    (window as any)['renderer'] = renderer;
+}
+
+if (window.document.readyState === 'complete') {
+    onload();
+} else {
+    window.onload = onload;
+}
